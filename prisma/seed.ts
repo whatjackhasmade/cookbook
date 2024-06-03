e6185480c5f73d8088d47fab8539629a66b24ae8
jackpritchard <jack.pritchard@sonovate.com>
2024-06-03 15:19:51 +0100

    feat: start allowing adding

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,14 +2,53 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function main() {
-	await prisma.recipe.create({
-		data: {
-			title: "Bangers and Mash",
-		},
-	});
+const recipes = [
+	{
+		name: "CHILLI GARLIC TOFU BAO 🌶️",
+		ingredients: [
+			{
+				name: "Tofu",
+				quantity: "200",
+				unit: "g",
+			},
+			{
+				name: "Bao buns",
+				quantity: "4",
+				unit: "",
+			},
+			{
+				name: "Chilli garlic sauce",
+				quantity: "1",
+				unit: "tbsp",
+			},
+		],
+		steps: [
+			{
+				content:
+					"1. for the dough: mix the yeast sugar & water in a jug then set aside to sit until foamy on top",
+			},
+		],
+	},
+];
 
-	console.log("Created recipe: Bangers and Mash");
+async function main() {
+	recipes.forEach(async (recipe) => {
+		console.log(`Creating recipe: ${recipe.name}`);
+
+		await prisma.recipe.create({
+			data: {
+				title: recipe.name,
+				ingredients: {
+					create: recipe.ingredients,
+				},
+				steps: {
+					create: recipe.steps,
+				},
+			},
+		});
+
+		console.log(`Created recipe: ${recipe.name}`);
+	});
 }
 
 main()
