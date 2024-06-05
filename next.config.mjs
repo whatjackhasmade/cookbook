59e3f015cb3cb9f187cf5160475cc8a96cb5a3d5
jackpritchard <jack.pritchard@sonovate.com>
2024-06-05 23:35:56 +0100

    feat: eslint

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   compiler: {
     styledComponents: true,
   },
+  output: "export",
 };
 
 export default nextConfig;
