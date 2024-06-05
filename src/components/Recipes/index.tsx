60e2def7a095e8d6464b965b69c899c56f6bc520
jackpritchard <jack.pritchard@sonovate.com>
2024-06-05 14:23:54 +0100

    feat: wrap

--- src/components/Recipes/index.tsx
+++ src/components/Recipes/index.tsx
@@ -2,13 +2,17 @@ import { recipes } from "@/data/recipes";
 import Link from "next/link";
 
 export default async function Recipes() {
-	return recipes.map((recipe) => (
-		<article key={recipe.slug}>
-			<Link href={`/recipes/${recipe.slug}`}>
-				<h2>
-					<span role="img">{recipe.emojis}</span> ~ {recipe.title}
-				</h2>
-			</Link>
-		</article>
-	));
+  return (
+    <>
+      {recipes.map((recipe) => (
+        <article key={recipe.slug}>
+          <Link href={`/recipes/${recipe.slug}`}>
+            <h2>
+              <span role="img">{recipe.emojis}</span> ~ {recipe.title}
+            </h2>
+          </Link>
+        </article>
+      ))}
+    </>
+  );
 }
